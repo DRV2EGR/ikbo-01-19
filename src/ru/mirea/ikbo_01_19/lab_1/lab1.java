f53a664ec0e5a7e7305d1d7afe0c89e830208131
DRV2EGR <1664286@gmail.com>
2020-09-03 21:43:18 +0300

    lab_1-Rogov

---
+++ src/ru/mirea/ikbo_01_19/lab_1/lab1.java
@@ -0,0 +1,10 @@
+package ru.mirea.ikbo_01_19.lab_1;
+
+
+public class lab1 {
+    public static void main(String []args) {
+        test_class make_tests = new test_class();
+        make_tests.make_test();
+    }
+}
+
